5da94368bbd8cd1cc01c58d56375deab37b92718
kailangl <157406748+kailangl@users.noreply.github.com>
2024-12-16 13:11:21 -0300

    tira underline

--- pages/index.tsx
+++ pages/index.tsx
@@ -102,25 +102,25 @@ const Home: NextPage = () => {
         <p className={styles.description}>{currentLang.description}</p>
 
         <div className={styles.grid}>
-          <Link href="/project/sites" passHref>
+          <Link href="/project/sites" passHref className={styles.rUnder}>
             <div className={styles.card}>
               <h2>{currentLang.pj1_title} &rarr;</h2>
               <p>{currentLang.pj1_description}</p>
             </div>
           </Link>
-          <Link href="/project/soundtracks" passHref>
+          <Link href="/project/soundtracks" passHref className={styles.rUnder}>
             <div className={styles.card}>
               <h2>{currentLang.pj2_title} &rarr;</h2>
               <p>{currentLang.pj2_description}</p>
             </div>
           </Link>
-          <Link href="/project/mods" passHref>
+          <Link href="/project/mods" passHref className={styles.rUnder}>
             <div className={styles.card}>
               <h2>{currentLang.pj3_title} &rarr;</h2>
               <p>{currentLang.pj3_description}</p>
             </div>
           </Link>
-          <Link href="/project/native" passHref>
+          <Link href="/project/native" passHref className={styles.rUnder}>
             <div className={styles.card}>
               <h2>{currentLang.pj4_title} &rarr;</h2>
               <p>{currentLang.pj4_description}</p>
