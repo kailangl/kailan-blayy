3dfb002d033ecbd4d89fb6eabb90acd6e6557586
kailangl <157406748+kailangl@users.noreply.github.com>
2024-12-28 22:32:03 -0300

    ssss
    asdcASDCDas

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -5,6 +5,29 @@ import styles from "../../styles/Home.module.css";
 import lang from "../../lang.json";
 import projects from "../../projects.json";
 
+const tracks = [
+  {
+    src: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1942645151&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    artist: "BlayySTK",
+    title: "Fantastic Mission",
+    artistLink: "https://soundcloud.com/blay-2k",
+    trackLink: "https://soundcloud.com/blay-2k/fantastic-mission",
+  },
+  {
+    src: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1763612727&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    artist: "BlayySTK",
+    title: "FutureTech Network",
+    artistLink: "https://soundcloud.com/blay-2k",
+    trackLink: "https://soundcloud.com/blay-2k/blayystk-futuretech-network",
+  },
+  {
+    src: "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1490040229&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true",
+    artist: "BlayySTK",
+    title: "The Phantom Garden",
+    artistLink: "https://soundcloud.com/blay-2k",
+    trackLink: "https://soundcloud.com/blay-2k/blayy-the-phantom-garden-m4a",
+  },
+];
 const ProjectPage = () => {
   const router = useRouter();
   const { id } = router.query;
@@ -44,20 +67,56 @@ const ProjectPage = () => {
   }
 
   return (
+    
     <div className={styles.container}>
+      
       <main className={bg}>
-        <h1 className={styles.title}>{project.title}</h1>
-        <p className={styles.description}>{project.description}</p>
-
-        <h2 className={styles.subtitle}>{currentLang.relatedProjects}</h2>
-        <div className={styles.grid}>
-          {project.relatedProjects.map((related, index) => (
-            <div key={index} className={styles.card}>
-              <h2>{related.title}</h2>
-              <p>{related.description}</p>
-            </div>
-          ))}
-        </div>
+        
+       {id == "soundtracks" ? (
+        <div>
+           <h1 className={styles.title}>- {currentLang.pj2_title} &rarr;</h1>
+           <p className={styles.description}>{currentLang.pj2_description}</p>
+ <div className={styles.container_m}>
+ 
+ {tracks.map((track, index) => (
+   <div key={index} className={styles.card}>
+     <iframe
+       width="100%"
+       height="200"
+       allow="autoplay"
+       src={track.src}
+       className={styles.iframe}
+     ></iframe>
+     <div className={styles.cardContent}>
+       <a
+         href={track.artistLink}
+         title={track.artist}
+         target="_blank"
+         rel="noopener noreferrer"
+         className={styles.link}
+       >
+         {track.artist}
+       </a>
+       <span> · </span>
+       <a
+         href={track.trackLink}
+         title={track.title}
+         target="_blank"
+         rel="noopener noreferrer"
+         className={styles.linkSecondary}
+       >
+         {track.title}
+       </a>
+     </div>
+   </div>
+   
+ ))}
+ </div>
+</div>
+) : ( 
+  <h1>nao tem arrombado</h1>
+  )}
+        
 
         <Link href="/">
           <button className={bgButton}>{currentLang.back}</button>
